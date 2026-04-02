bb607fcced7f871a97510f693ea67d53e70c78c7
ChengLi8888 <1002607666@qq.com>
2026-04-02 17:28:00 +0800

    首页重定向到学习页面

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
+// 允许所有路由公开访问，不强制登录
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/stripe"],
+  publicRoutes: ["/(.*)"],
 });
- 
+
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
